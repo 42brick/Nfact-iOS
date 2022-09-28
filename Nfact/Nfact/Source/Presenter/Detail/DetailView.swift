e85d9262aaca9b954ab8ffb5f82c518b1a31d1a2
송영모 <mooyoung2309@gmail.com>
2022-09-28 19:35:06 +0900

    feat: 상세 페이지 UI 구성

--- Nfact/Nfact/Source/Presenter/Detail/DetailView.swift
+++ Nfact/Nfact/Source/Presenter/Detail/DetailView.swift
@@ -8,21 +8,23 @@
 import SwiftUI
 
 struct DetailView: View {
+    let dummy = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+    
     var body: some View {
         ScrollView {
-            HStack {
-                Image(systemName: "paperplane.circle.fill")
-                    .resizable()
-                    .frame(width: 100, height: 100)
-                    .padding(.trailing, 10)
-                
-                VStack(alignment: .leading) {
-                    Text("name")
-                        .font(.title2)
-                        .bold()
-                        .foregroundColor(.black)
+            VStack(alignment: .leading) {
+                HStack {
+                    Image(systemName: "paperplane.circle.fill")
+                        .resizable()
+                        .frame(width: 100, height: 100)
+                        .padding(.trailing, 10)
                     
-                    HStack {
+                    VStack(alignment: .leading) {
+                        Text("name")
+                            .font(.title2)
+                            .bold()
+                            .foregroundColor(.black)
+                        
                         Text("XXXXX")
                             .font(.caption)
                             .foregroundColor(.gray)
@@ -32,24 +34,45 @@ struct DetailView: View {
                         Button() {
                             
                         } label: {
-                            Image(systemName: "doc.on.doc")
-                                .imageScale(.small)
+                            Text("토큰 주소 복사")
+                                .font(.subheadline)
                         }
-                        .buttonStyle(.plain)
-                        .tint(.blue)
+                        .buttonStyle(.bordered)
+                        .tint(.green)
                     }
+                    .frame(height: 100)
                     
                     Spacer()
                 }
-                .frame(height: 100)
+                
+                Divider()
+                    .padding(.vertical)
+                
+                ScrollView(.horizontal,showsIndicators: false) {
+                    HStack(spacing: 15) {
+                        ForEach(dummy,id: \.self) { user in
+                            InfoItemView()
+                                .frame(width: 60)
+                            
+                            Divider()
+                        }
+                    }
+                }
+                
+                VStack(alignment: .leading, spacing: 10) {
+                    Text("상세 정보")
+                        .font(.title3)
+                        .bold()
+                    
+                    Text("세부 내용입니다. 어쩌구 저쩌구")
+                        .font(.subheadline)
+                        .fontWeight(.medium)
+                        .foregroundColor(.gray)
+                }
+                .padding(.vertical)
                 
                 Spacer()
             }
-            
-            Divider()
-                .padding(.vertical)
-            
-            Spacer()
         }
         .padding(.horizontal)
         .navigationBarTitleDisplayMode(.inline)
