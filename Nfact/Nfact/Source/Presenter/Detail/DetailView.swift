6f5f0785896314841436cb0bee14edd9d339018c
송영모 <mooyoung2309@gmail.com>
2022-09-28 14:03:18 +0900

    feat: 상세페이지 상단 바 생성

--- Nfact/Nfact/Source/Presenter/Detail/DetailView.swift
+++ Nfact/Nfact/Source/Presenter/Detail/DetailView.swift
@@ -9,9 +9,49 @@ import SwiftUI
 
 struct DetailView: View {
     var body: some View {
-        VStack {
-            Text("상세 페이지")
+        ScrollView {
+            HStack {
+                Image(systemName: "paperplane.circle.fill")
+                    .resizable()
+                    .frame(width: 100, height: 100)
+                    .padding(.trailing, 10)
+                
+                VStack(alignment: .leading) {
+                    Text("name")
+                        .font(.title2)
+                        .bold()
+                        .foregroundColor(.black)
+                    
+                    HStack {
+                        Text("XXXXX")
+                            .font(.caption)
+                            .foregroundColor(.gray)
+                        
+                        Spacer()
+                        
+                        Button() {
+                            
+                        } label: {
+                            Image(systemName: "doc.on.doc")
+                                .imageScale(.small)
+                        }
+                        .buttonStyle(.plain)
+                        .tint(.blue)
+                    }
+                    
+                    Spacer()
+                }
+                .frame(height: 100)
+                
+                Spacer()
+            }
+            
+            Divider()
+                .padding(.vertical)
+            
+            Spacer()
         }
-        .padding()
+        .padding(.horizontal)
+        .navigationBarTitleDisplayMode(.inline)
     }
 }
